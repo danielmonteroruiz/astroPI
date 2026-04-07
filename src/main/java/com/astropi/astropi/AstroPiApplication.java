3b6ceb6cb32a4ace450529142e40c1fa53d9a2f6
Dani Montero <dmr.coding@protonmail.com>
2026-04-07 10:25:43 +0200

    initial Project Setup

---
+++ src/main/java/com/astropi/astropi/AstroPiApplication.java
@@ -0,0 +1,13 @@
+package com.astropi.astropi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstroPiApplication.class, args);
+    }
+
+}
